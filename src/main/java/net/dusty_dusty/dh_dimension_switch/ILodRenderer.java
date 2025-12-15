9ae25d24540a61aa08941738017d3ab999b8b553
ada_aster <an.ada.poirier@gmail.com>
2025-12-15 16:52:21 -0500

    Enable Files

--- src/main/java/net/dusty_dusty/dh_dimension_switch/ILodRenderer.java
+++ src/main/java/net/dusty_dusty/dh_dimension_switch/ILodRenderer.java
@@ -1,11 +1,11 @@
-//package net.dusty_dusty.dh_dimension_switch;
-//
-//import com.seibel.distanthorizons.api.interfaces.override.rendering.IDhApiShaderProgram;
-//import com.seibel.distanthorizons.core.render.RenderBufferHandler;
-//import com.seibel.distanthorizons.core.render.renderer.RenderParams;
-//
-//public interface ILodRenderer {
-//
-//    void dhds$renderLodPass(IDhApiShaderProgram shaderProgram, RenderBufferHandler lodBufferHandler, RenderParams renderEventParam, boolean opaquePass);
-//
-//}
+package net.dusty_dusty.dh_dimension_switch;
+
+import com.seibel.distanthorizons.api.interfaces.override.rendering.IDhApiShaderProgram;
+import com.seibel.distanthorizons.core.render.RenderBufferHandler;
+import com.seibel.distanthorizons.core.render.renderer.RenderParams;
+
+public interface ILodRenderer {
+
+    void dhds$renderLodPass(IDhApiShaderProgram shaderProgram, RenderBufferHandler lodBufferHandler, RenderParams renderEventParam, boolean opaquePass);
+
+}
