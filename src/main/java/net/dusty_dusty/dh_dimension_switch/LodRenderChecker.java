4d9b881b2ddf4bf1c8ce623bb43618f4311e494f
ada_aster <an.ada.poirier@gmail.com>
2025-12-15 00:23:56 -0500

    Fix quick mistake which causes rendering to always be false

--- src/main/java/net/dusty_dusty/dh_dimension_switch/LodRenderChecker.java
+++ src/main/java/net/dusty_dusty/dh_dimension_switch/LodRenderChecker.java
@@ -16,6 +16,7 @@ public class LodRenderChecker {
         assert CLIENT.player != null;
         if( !CLIENT.level.dimensionType().hasCeiling() ) {
             shouldRun = !CLIENT.level.dimension().equals( Level.OVERWORLD ) || CLIENT.player.position().y > 40;
+            return;
         }
         shouldRun = false;
     }
