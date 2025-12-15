5199ccb8876f47e95b7061d4cc82a85702c712aa
ada_aster <an.ada.poirier@gmail.com>
2025-12-15 17:12:49 -0500

    Fix and enable fade suppression

--- src/main/java/net/dusty_dusty/dh_dimension_switch/mixins/MixinFadeRenderer.java
+++ src/main/java/net/dusty_dusty/dh_dimension_switch/mixins/MixinFadeRenderer.java
@@ -1,23 +1,23 @@
 package net.dusty_dusty.dh_dimension_switch.mixins;
 
-import com.seibel.distanthorizons.core.render.renderer.FadeRenderer;
+import com.seibel.distanthorizons.core.render.renderer.DhFadeRenderer;
 import com.seibel.distanthorizons.core.render.renderer.shaders.FadeApplyShader;
-import com.seibel.distanthorizons.core.render.renderer.shaders.FadeShader;
+import com.seibel.distanthorizons.core.render.renderer.shaders.DhFadeShader;
 import net.dusty_dusty.dh_dimension_switch.LodRenderChecker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin( FadeRenderer.class )
+@Mixin( DhFadeRenderer.class )
 public class MixinFadeRenderer {
 
     @Redirect( method = "render", at = @At(
             value = "INVOKE",
-            target = "Lcom/seibel/distanthorizons/core/render/renderer/shaders/FadeShader;render(F)V"
+            target = "Lcom/seibel/distanthorizons/core/render/renderer/shaders/DhFadeShader;render(F)V"
         ), remap = false
     )
     void renderProxy(
-            FadeShader instance,
+            DhFadeShader instance,
             float v
     ){
         if ( LodRenderChecker.shouldRender() ) instance.render( v );
