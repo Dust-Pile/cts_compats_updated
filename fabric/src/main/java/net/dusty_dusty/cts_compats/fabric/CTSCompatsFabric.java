45c1522938ef5be6ab44395dfe98abc0a3486ad1
AlmanaX-21 <98935792+AlmanaX-21@users.noreply.github.com>
2026-09-02 21:00:54 +0530

    build: create Architectury module layout

---
+++ fabric/src/main/java/net/dusty_dusty/cts_compats/fabric/CTSCompatsFabric.java
@@ -0,0 +1,9 @@
+package net.dusty_dusty.cts_compats.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public final class CTSCompatsFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ fabric/src/main/java/net/dusty_dusty/cts_compats/fabric/CTSCompatsFabricClient.java
@@ -0,0 +1,9 @@
+package net.dusty_dusty.cts_compats.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CTSCompatsFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
